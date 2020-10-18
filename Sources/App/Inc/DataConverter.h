5557024ab824520d97d67f27aa582ece96440327
yosmanyhs <yosmanyhs8810@outlook.com>
2020-10-18 19:48:05 -0400

    Added methods to DataConverter class to convert from integer to string and to convert from float to string with limited character count.

--- Sources/App/Inc/DataConverter.h
+++ Sources/App/Inc/DataConverter.h
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define NUM_TO_STR_BUF_SIZE     32
+
 class DataConverter
 {
     public:
@@ -14,10 +16,15 @@ class DataConverter
         static float StringToFloat2(char* text, char** stopPos, uint32_t * success);
         static int StringToInteger2(char* text, char** stopPos, uint32_t * success);
     
+        static const char* FloatToStringTruncate(float f, int maxlen = 0);
+        static const char* IntegerToString(int32_t value);
+    
     protected:
         static const uint32_t BIT_NEGATIVE = (1 << 15);
         static const uint32_t BIT_DIGITS   = (1 << 1);
         static const uint32_t BIT_DECIMAL   = (1 << 0);
+    
+        static char NumberToStrBuffer[NUM_TO_STR_BUF_SIZE];
 };
 
 #endif
--- Sources/App/Src/DataConverter.cpp
+++ Sources/App/Src/DataConverter.cpp
@@ -1,5 +1,7 @@
 #include "DataConverter.h"
 
+char DataConverter::NumberToStrBuffer[NUM_TO_STR_BUF_SIZE];
+
 //////////////////////////////////////////////////////////////////////////////////////////
 
 float DataConverter::StringToFloat(char** text, uint32_t * success)
@@ -210,5 +212,111 @@ int DataConverter::StringToInteger2(char* text, char** stopPos, uint32_t * succe
 
     *stopPos = text;
 	return result;
+}
+
+// This method is not thread safe. It returns a pointer to static data
+// This data will be modified in a new call to FloatToStringTruncate or 
+// IntegerToString. 
+
+// The number format has 4 digits of precision
+// The maxlen parameter allows to 'cut' the resulting text to the specified
+// number of characters, including sign. In the case the last character after
+// the text being truncated is a dot (.) then, it is also removed.
+const char* DataConverter::FloatToStringTruncate(float f, int maxlen /* = 0 */)
+{
+    int i;
+    unsigned long z, k;
+    char *c = &DataConverter::NumberToStrBuffer[NUM_TO_STR_BUF_SIZE - 1];    // Point to the last position of buffer
+    float f2,t;
+    int   sign = 0;
+
+    /* Unsigned long long only allows for 20 digits of precision
+    * which is already more than double supports, so we limit the
+    * digits to this.  long double might require an increase if it is ever
+    * implemented.
+    */
+    if (f < 0.0f) 
+    {
+        sign = 1;
+        f = -f;
+    }
+
+    k = f + 0.0001f;
+    f2 = f - k;
+
+    i = 1;
+ 
+    while (f >= 10.0f) 
+    {
+        f /= 10.0f;
+        i++;
+    }
+
+    // Terminate string
+    *(c--)= '\0';
+
+    t = f2 * 10000.0f;
+    z = t + 0.5f;
+    
+    for (i = 0; i < 4; i++)
+    {
+        *(c--) = ('0' + (z % 10));
+        z /= 10;
+    }
+    
+    *(c--) = ('.');
+    
+    do 
+    {
+        *(c--) = ('0'+ (k % 10));
+        k /= 10;
+    }
+    while (k);
+
+    if (sign)
+        *(c--) = ('-');
+    
+    if (maxlen > 0)
+    {
+        c[maxlen + 1] = '\0';
+
+        // verificar que el ultimo caracter no sea el punto. si asi fuera eliminarlo
+        if (c[maxlen] == '.')
+            c[maxlen] = '\0';
+    }
+
+    return ++c;
+}
+
+// This method is not thread safe. It returns a pointer to static data
+// This data will be modified in a new call to FloatToString or 
+// IntegerToString. 
+const char* DataConverter::IntegerToString(int32_t value)
+{
+    char *c;
+    int sign = 0;
+
+    c = &DataConverter::NumberToStrBuffer[NUM_TO_STR_BUF_SIZE - 1];
+
+    if (value < 0) 
+    {
+        /* negative, so negate */
+        value = -value;
+    }
+    
+    // Terminate string
+    *(c--)= '\0';
+    
+    do 
+    {
+        *(c--) = ('0'+ (value % 10));
+        value /= 10;
+    }
+    while (value != 0);
+
+    if (sign)
+        *(c--) = ('-');
+    
+    return c;
 }
 
